cb8ace6255e80633defd61424ed622e7588c3a5a
Atul Ghate <atulghate155@gmail.com>
2021-08-30 15:38:13 +0530

    added comments link

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,31 +12,60 @@ session_start();
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,300&family=Roboto:ital,wght@0,400;0,700;1,300;1,400&display=swap" rel="stylesheet"> 
+   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,300&family=Roboto:ital,wght@0,400;0,700;1,300;1,400&display=swap" rel="stylesheet"> 
 </head>
-<body>
-<div class="navigation">
+<body>  
+<nav>
+     
+     <input type="checkbox"  id="check">
+     <label for="check" class="checkbtn">
+     <i class="fas fa-bars"></i> 
+     </label>
+     <label class="logo">iBLOG</label>
     
-    <div class="left">
-    <h2>iBlog</h2>
-   <a href="index.php">Home</a>
-    </div>
- <div class="right">
- <ul>
-  <li><a href="logout.php">Logout</a></li>
-
-  <?php
- if(isset($_SESSION['typee'])){
- ?> <li><a href="admin_dash.php">Admin</a></li>
- <li><a href="logout.php">Logout</a></li>
- <?php
-}?>
-
-</ul>
- </div>
+       <ul>
+       
+       <li> <a class="active" href="index.php">Home</a></li>
+       <li><a href="contact.php">Contact Us</a></li>
+     <!-- <li><a href="logout.php">Logout</a></li> -->
+     <?php
+    if(isset($_SESSION['typee'])){
+    ?> 
+     <li><a href="admin_dash.php">Admin</a></li>
+     <li><a href="logout.php">Logout</a></li>
+   
+     <?php
+    }
+   ?> 
+   <?php
+     if(isset($_SESSION['pass'])){
+     ?>
+     
+    <li><a href="author_dash.php">Author</a></li>
+    <li><a href="author_info.php">My Details</a></li>
+    <li><a href="logout.php">Logout</a></li>
+    <?php
+   }
+    if( $_SESSION['pass']  ==false){
+     ?> 
+     
+     <li><a href="login.php">login</a></li>
+      <li><a href="registretion.php">Register </a></li>
  
-</div>
-<div class="content">
+             
+<?php
+}
+?>
+</ul>
+<div class="iconn">
+      <img src="sun.png" id="icon">
+       </div>
+
+</nav> 
+
+
+  
 <?php
 include("dbcon.php");
   $rid = $_REQUEST['id'];
@@ -44,23 +73,37 @@ include("dbcon.php");
  $qry="SELECT * FROM `post` WHERE cid = $rid";
  $run = mysqli_query($conn,$qry);
  foreach( $run as $f){
-   ?> <div class="title">
-      <h1> <?php echo $f['title'];?></h1>  
+   ?> 
+    
+    <div class="mcontent">
+
+<div class="lcontent">
+<div class="imgee">
+   <div class="title">
+     <h2> <?php  echo $f['title'];?></h2>  
   </div>
+  <div class="time">
+     Posted On : <?php echo $f['datetime'];?>
+     </div>
+     <img src="imagess\<?php echo $f['image'];?>">
+     </div>
+   </div>
+<div class="content">
     <div class="desc">
    <?php echo $f['content'];?>
    </div>
-    <div class="time">
-     Date&time:<?php echo $f['datetime'];?> 
-     </div>
+   
     
     <div class="btnn">
       <a href="index.php">back</a>
+      <a href="comment1.php?id=<?php echo $rid;?>"  name="edit"> COMMENTS&nbsp; </a>
+
     </div>
-  <hr>
+  
    </div>
   <?php } ?>
-        
+     <hr>   
+</div>
 </div>
 </body>
 </html>
